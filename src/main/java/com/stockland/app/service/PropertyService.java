8269b616d7e4cbb77845201afa44e8e1f83bc208
Arnas Kičas <arnas.kicas@mif.stud.vu.lt>
2026-02-16 12:51:13 +0200

    Added entity and repository for entity

--- src/main/java/com/stockland/app/service/PropertyService.java
+++ src/main/java/com/stockland/app/service/PropertyService.java
@@ -1,4 +1,16 @@
 package com.stockland.app.service;
 
+import com.stockland.app.model.PropertyRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+    public PropertyService(PropertyRepository propertyRepository){
+        this.propertyRepository = propertyRepository;
+    }
+
+
 }
